ca944872dfe22097ebcb074bf4ddbf392dd7f280
Theo Touzet <theo.touzet@epitech.eu>
2019-02-23 14:47:28 +0100

    Added swagger file. Added swagger UI on route /swagger.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const logger = require('koa-logger');
+const swagger = require('swagger2');
+const { ui } = require('swagger2-koa');
 
 
 const app = new Koa();
 const router = new Router();
+
+const swaggerDoc = swagger.loadDocumentSync('./swagger.yaml');
 
 router.get('/', (ctx) => {
   ctx.body = 'Hello World!';
@@ -13,6 +17,8 @@ router.get('/', (ctx) => {
 app.use(router.routes());
 app.use(logger());
 app.use(router.allowedMethods());
+app.use(ui(swaggerDoc, '/swagger'));
+
 const server = app.listen(3000);
 
 module.exports = server;
