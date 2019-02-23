7c485bec380278ccc216ea702f5196a811ecee5c
alexandre joris <alexandre.joris@epitech.eu>
2019-02-23 16:23:46 +0100

    added Mongoose: MongoConnection, models/users ; dotenv: .env + exemple

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 const logger = require('koa-logger');
-
+const db = require('./models');
 
 const app = new Koa();
 const router = new Router();
@@ -10,9 +12,17 @@ router.get('/', (ctx) => {
   ctx.body = 'Hello World!';
 });
 
+router.post('/subscribe', (ctx) => {
+  const form = ctx.request.body;
+
+  console.log('ctx.request.body = ', form);
+});
+
 app.use(router.routes());
 app.use(logger());
+app.use(bodyParser());
 app.use(router.allowedMethods());
+
 const server = app.listen(3000);
 
 module.exports = server;
