3332ad4221b359c31cda71ed8c5125feb9efd543
shadonovitch <alexandre.joris@gmail.com>
2019-04-11 19:36:07 +0200

    Feature hashtag woofs (#47)
    * added get /users?handle= route with partial matching
    
    * added hashtag feature + tests

--- src/db/models/woof.js
+++ src/db/models/woof.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const woofSchema = mongoose.Schema({
   handle: String,
   text: String,
+  hashtags: [{
+    type: String,
+  }],
 });
 
 module.exports = woofSchema;
